4891a4dcdefec5f2aeb13227c9a2d6366fe9d36f
Aishita Saxena <117641838+aishita30@users.noreply.github.com>
2024-09-03 10:04:44 +0530

    Update dbConnection.js

--- backend/dbConnection.js
+++ backend/dbConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const uri =
-  "mongodb+srv://aditya:aditya@cluster0.htk0w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://admin:adminhoon@cluster0.d4nok.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 function dbConnection() {
   mongoose
